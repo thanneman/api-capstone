3c1fcb01c476e6fe859a56e7014c88f7ee20597f
Travis Hanneman <thanneman1@gmail.com>
2019-10-27 21:01:01 -0700

    Added YouTube API

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ function displayResults(responseJson) {
     //Clear error message and previous results
     $('#js-error-message').empty();
     $('#js-found-movies').empty();
+    $('#movie-poster').empty();
+    $('#movie-list').empty();
     for (let i = 0; i < responseJson.Search.length; i++){
         $('#js-found-movies').append(
             `<option value="${responseJson.Search[i].imdbID}">${responseJson.Search[i].Title}, (${responseJson.Search[i].Year})</option>`
@@ -21,13 +23,14 @@ function displayResults(responseJson) {
 };
 
 //Applies user's input into the api URL and checks for a 200 status
-function findMovie(baseUrl, searchTerm) {
+function findMovie(searchTerm) {
+    const findUrl = 'http://www.omdbapi.com/?apikey=409a9a19&'
     const params = {
         s: searchTerm
     };
 
     const queryString = formatQueryParams(params);
-    const url = baseUrl + queryString;
+    const url = findUrl + queryString;
     console.log(url);
     fetch(url)
         .then(response => {
@@ -46,29 +49,30 @@ function findMovie(baseUrl, searchTerm) {
 function watchForm() {
   $('#js-form').submit(event => {
     event.preventDefault();
-    const baseUrl = 'http://www.omdbapi.com/?apikey=409a9a19&'
     const searchTerm = $('#js-search-term').val();
-    findMovie(baseUrl, searchTerm);
+    findMovie(searchTerm);
+    getYouTubeVideos(searchTerm);
   });
 }
 
 $(watchForm);
 
+
+
+
+//Starts second half of movie search
 function watchSecondForm() {
     $('#js-form-movies').submit(event => {
       event.preventDefault();
-      const baseUrl = 'http://www.omdbapi.com/?apikey=409a9a19&'
       const searchVal = $('#js-found-movies').val();
-      getMovie(baseUrl, searchVal);
+      getMovie(searchVal);
     });
-  }
+}
 
 //Creates the HTML for each repo and appends to element in the DOM
 function displayMovie(responseJson) {
     console.log(responseJson);
-    //Clear error message and previous results
-    $('#js-error-message').empty();
-    $('#js-found-movies').empty();
+
     $('#movie-poster').append(
         `<img src="${responseJson.Poster}" alt="${responseJson.Title} movie poster">`);
     $('#movie-list').append(
@@ -82,18 +86,20 @@ function displayMovie(responseJson) {
             <p>IMDb Rating: ${responseJson.imdbRating} / 10</p>`);
     $('#results').addClass('hidden');
     $('#movie-results').removeClass('hidden');
+    $('#movie-trailer').removeClass('hidden');
 };
 
 //Applies user's input into the api URL and checks for a 200 status
-function getMovie(baseUrl, searchVal) {
+function getMovie(searchVal) {
+    const getUrl = 'http://www.omdbapi.com/?apikey=409a9a19&'
     const params = {
         i: searchVal
     };
 
     const queryString = formatQueryParams(params);
-    const url = baseUrl + queryString;
-    console.log(url);
-    fetch(url)
+    const secondUrl = getUrl + queryString;
+    console.log(secondUrl);
+    fetch(secondUrl)
         .then(response => {
             if (response.ok) {
                 return response.json();
@@ -105,3 +111,44 @@ function getMovie(baseUrl, searchVal) {
           $('#js-error-message').text(`Something went wrong: ${err.message}`);
         });
 }
+
+
+
+
+//YouTube API for Trailer
+function displayYoutube(responseJson) {
+    console.log(responseJson);
+    for (let i = 0; i < responseJson.items.length; i++){
+      $('#movie-trailer').append(
+        `<iframe width="560" height="315" src="https://www.youtube.com/embed/${responseJson.items[i].id.videoId}" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`
+    )};
+  };
+  
+  function getYouTubeVideos(query, maxResults=1) {
+    const apiKey = 'AIzaSyB9Avb3cXTPYRtPK_eCTIYp699EiyePoxA'; 
+    const youtubeURL = 'https://www.googleapis.com/youtube/v3/search';
+
+    const params = {
+      key: apiKey,
+      q: query + ' trailer',
+      part: 'snippet',
+      maxResults,
+      type: 'video'
+    };
+    const queryString = formatQueryParams(params)
+    const url = youtubeURL + '?' + queryString;
+  
+    console.log(url);
+  
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => displayYoutube(responseJson))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      });
+  }
